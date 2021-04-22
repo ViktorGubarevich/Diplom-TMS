1e19a37107437b8bd48e9fed8a5e60ce5a7fb296
Viktor Gubarevich <gubarew.v@gmail.com>
2021-04-23 01:11:19 +0300

    feat: change SelectForm and MainPageForm

--- coffeetime/src/pages/Location/SelectForm.js
+++ coffeetime/src/pages/Location/SelectForm.js
@@ -1,70 +1,129 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Select from "../../components/Select";
 import "./style.scss";
 
-class SelectForm extends Component {
-  constructor(props) {
-    super(props);
+const SelectForm = (props) => {
+  let [state, setState] = useState({
+    newUser: {
+      place: "",
+    },
+  });
 
-    this.state = {
-      newUser: {
-        place: "",
-      },
+  placeForMainPageForm = state.newUser.place;
 
-      placeOptions: [
-        {
-          id: 1,
-          name: "Мстиславца, 8",
-          rout: "mstislavca",
-        },
-        {
-          id: 2,
-          name: "Пр. Победителей, 7",
-          rout: "pobeditelei7",
-        },
-      ],
-    };
+  let placeOptions = [
+    {
+      id: 1,
+      name: "Мстиславца, 8",
+      rout: "mstislavca",
+    },
+    {
+      id: 2,
+      name: "Пр. Победителей, 7",
+      rout: "pobeditelei7",
+    },
+  ];
 
-    this.handleInput = this.handleInput.bind(this);
-  }
-
-  handleInput(e) {
+  function handleInput(e) {
     let value = e.target.value;
     let name = e.target.name;
-    this.setState((prevState) => ({
+    setState({
       newUser: {
-        ...prevState.newUser,
         [name]: value,
       },
-    }));
+    });
   }
 
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <main className="main">
-        <div className="main-data">
-          <div className="main-data-personal">
-            <Select
-              title={"Place"}
-              name={"place"}
-              options={this.state.placeOptions}
-              value={this.state.newUser.place}
-              placeholder={"Выберите торговый объект"}
-              handleChange={this.handleInput}
-            />
-          </div>
-          <button
-            className="button"
-            type="submit"
-            onClick={() => handleSubmit(this.state.newUser.place)}
-          >
-            Открыть смену
-          </button>
+  const handleSubmit = props.handleSubmit;
+
+  return (
+    <main className="main">
+      <div className="main-data">
+        <div className="main-data-personal">
+          <Select
+            title={"Place"}
+            name={"place"}
+            options={placeOptions}
+            value={state.newUser.place}
+            placeholder={"Выберите торговый объект"}
+            handleChange={(e) => handleInput(e)}
+          />
         </div>
-      </main>
-    );
-  }
-}
+        <button
+          className="button"
+          type="submit"
+          onClick={() => handleSubmit(state.newUser.place)}
+        >
+          Открыть смену
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export let placeForMainPageForm;
 
 export default SelectForm;
+
+// class SelectForm extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       newUser: {
+//         place: "",
+//       },
+
+//       placeOptions: [
+//         {
+//           id: 1,
+//           name: "Мстиславца, 8",
+//           rout: "mstislavca",
+//         },
+//         {
+//           id: 2,
+//           name: "Пр. Победителей, 7",
+//           rout: "pobeditelei7",
+//         },
+//       ],
+//     };
+//   }
+
+//   handleInput(e) {
+//     let value = e.target.value;
+//     let name = e.target.name;
+//     this.setState((prevState) => ({
+//       newUser: {
+//         ...prevState.newUser,
+//         [name]: value,
+//       },
+//     }));
+//   }
+
+//   render() {
+//     const { handleSubmit } = this.props;
+//     return (
+//       <main className="main">
+//         <div className="main-data">
+//           <div className="main-data-personal">
+//             <Select
+//               title={"Place"}
+//               name={"place"}
+//               options={this.state.placeOptions}
+//               value={this.state.newUser.place}
+//               placeholder={"Выберите торговый объект"}
+//               handleChange={(e) => this.handleInput(e)}
+//             />
+//           </div>
+//           <button
+//             className="button"
+//             type="submit"
+//             onClick={() => handleSubmit(this.state.newUser.place)}
+//           >
+//             Открыть смену
+//           </button>
+//         </div>
+//       </main>
+//     );
+//   }
+// }
--- coffeetime/src/pages/MainPage/MainPageForm.js
+++ coffeetime/src/pages/MainPage/MainPageForm.js
@@ -2,14 +2,27 @@ import React from "react";
 import Button from "../../components/Button";
 import Clock from "../../components/Clock";
 import { Link } from "react-router-dom";
+
+import { placeForMainPageForm } from "../Location/SelectForm";
 import "./style.scss";
 
 function MainPageForm() {
+  function placeObject() {
+    if (placeForMainPageForm === "mstislavca") {
+      return "Текущий объект: Мстиславца, 8";
+    }
+    if (placeForMainPageForm === "pobeditelei7") {
+      return "Текущий объект: Пр. Победителей, 7";
+    }
+  }
+
   return (
     <div>
       <header className="header">
         <div className="header-data">
-          <div className="header-data_user">Торговый объект: Мстиславца, 8</div>
+          <div className="header-data_user">
+            {placeObject(placeForMainPageForm)}
+          </div>
           <div className="header-data_user">Текущий пользователь: Виктор</div>
           <Clock />
         </div>
@@ -22,28 +35,28 @@ function MainPageForm() {
             </Link>
           </div>
           <div>
-            <Link to="../place/mstislavca/777" className="grid-button">
+            <Link to="/" className="grid-button">
               Учет рабочего времени
             </Link>
           </div>
           <Button type="submit">Закрыть смену</Button>
           <div>
-            <Link to="../place/mstislavca/777" className="grid-button">
+            <Link to="/" className="grid-button">
               Закрытые заказы
             </Link>
           </div>
           <div>
-            <Link to="../place/mstislavca/777" className="grid-button">
+            <Link to="/" className="grid-button">
               Меню
             </Link>
           </div>
           <div>
-            <Link to="../place/mstislavca/777" className="grid-button">
+            <Link to="/" className="grid-button">
               Отчет
             </Link>
           </div>
           <div id="admin-box7">
-            <Link to="../place/mstislavca/777" className="grid-button">
+            <Link to="/" className="grid-button">
               Склад
             </Link>
           </div>
